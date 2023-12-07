d3fef3d46f2b7b720f81c1e1ee6df0df5bce64ec
evollve <adamcw@gmail.com>
2023-12-06 23:00:42 -0600

    commenting out old js

--- script.js
+++ script.js
@@ -44,32 +44,32 @@ function updateTimer() {
     document.getElementById('countdown').textContent = numberWithCommas(daysToGo) + ' days';
 }
 
-function updateLifeProgress() {
-    if (!birthDate) return;
+// function updateLifeProgress() {
+//     if (!birthDate) return;
 
-    const now = new Date();
-    const expectedLifespan = 77;
-    const endDate = new Date(birthDate.getFullYear() + expectedLifespan, birthDate.getMonth(), birthDate.getDate());
-    const totalLifeSpan = endDate - birthDate;
-    const livedLifeSpan = now - birthDate;
-    const lifeProgress = Math.min(Math.max((livedLifeSpan / totalLifeSpan) * 100, 0), 100);
+//     const now = new Date();
+//     const expectedLifespan = 77;
+//     const endDate = new Date(birthDate.getFullYear() + expectedLifespan, birthDate.getMonth(), birthDate.getDate());
+//     const totalLifeSpan = endDate - birthDate;
+//     const livedLifeSpan = now - birthDate;
+//     const lifeProgress = Math.min(Math.max((livedLifeSpan / totalLifeSpan) * 100, 0), 100);
 
-    const progressBar = document.getElementById('lifeProgressBar');
-    progressBar.style.width = lifeProgress + '%';
-    document.getElementById('lifeProgressPercentage').textContent = lifeProgress.toFixed(2) + '% of your life completed';
+//     const progressBar = document.getElementById('lifeProgressBar');
+//     progressBar.style.width = lifeProgress + '%';
+//     document.getElementById('lifeProgressPercentage').textContent = lifeProgress.toFixed(2) + '% of your life completed';
 
-    const svgWidth = document.getElementById('lifespanTimeline').clientWidth;
-    const progressPosition = (svgWidth * lifeProgress) / 100;
+//     const svgWidth = document.getElementById('lifespanTimeline').clientWidth;
+//     const progressPosition = (svgWidth * lifeProgress) / 100;
 
-    // Make the emoji visible and position it along the timeline
-    const icon = document.getElementById('lifeProgressIcon');
-    icon.style.display = 'block';
-    icon.setAttribute('x', progressPosition);
-    const age = now.getFullYear() - birthDate.getFullYear();
-    const currentAgeText = document.getElementById('currentAgeText');
-    currentAgeText.textContent = `${age} years old`;
-    currentAgeText.setAttribute('x', progressPosition);
-}
+//     // Make the emoji visible and position it along the timeline
+//     const icon = document.getElementById('lifeProgressIcon');
+//     icon.style.display = 'block';
+//     icon.setAttribute('x', progressPosition);
+//     const age = now.getFullYear() - birthDate.getFullYear();
+//     const currentAgeText = document.getElementById('currentAgeText');
+//     currentAgeText.textContent = `${age} years old`;
+//     currentAgeText.setAttribute('x', progressPosition);
+// }
 
 function displayAge(birthdate) {
     const now = new Date();
@@ -81,21 +81,21 @@ function displayAge(birthdate) {
     document.getElementById('age').textContent = `${age} years old`;
 }
 
-function displayMilestones(birthdate) {
-    const now = new Date();
-    const milestones = {
-        '16th Birthday (Driving Age)': new Date(birthdate.getFullYear() + 16, birthdate.getMonth(), birthdate.getDate()),
-        '18th Birthday (Voting Age)': new Date(birthdate.getFullYear() + 18, birthdate.getMonth(), birthdate.getDate()),
-        '21st Birthday (Drinking Age)': new Date(birthdate.getFullYear() + 21, birthdate.getMonth(), birthdate.getDate()),
-        'Retirement Age (67 years)': new Date(birthdate.getFullYear() + 67, birthdate.getMonth(), birthdate.getDate())
-    };
+// function displayMilestones(birthdate) {
+//     const now = new Date();
+//     const milestones = {
+//         '16th Birthday (Driving Age)': new Date(birthdate.getFullYear() + 16, birthdate.getMonth(), birthdate.getDate()),
+//         '18th Birthday (Voting Age)': new Date(birthdate.getFullYear() + 18, birthdate.getMonth(), birthdate.getDate()),
+//         '21st Birthday (Drinking Age)': new Date(birthdate.getFullYear() + 21, birthdate.getMonth(), birthdate.getDate()),
+//         'Retirement Age (67 years)': new Date(birthdate.getFullYear() + 67, birthdate.getMonth(), birthdate.getDate())
+//     };
 
-    let milestoneListHtml = '';
-    for (let milestone in milestones) {
-        const date = new Date(milestones[milestone]);
-        const isPast = now > date;
-        milestoneListHtml += `<strong>${milestone}:</strong> ${date.toLocaleDateString()}${isPast ? ' (Passed)' : ''}<br>`;
-    }
+//     let milestoneListHtml = '';
+//     for (let milestone in milestones) {
+//         const date = new Date(milestones[milestone]);
+//         const isPast = now > date;
+//         milestoneListHtml += `<strong>${milestone}:</strong> ${date.toLocaleDateString()}${isPast ? ' (Passed)' : ''}<br>`;
+//     }
 
-    document.getElementById('milestoneList').innerHTML = milestoneListHtml;
-}
+//     document.getElementById('milestoneList').innerHTML = milestoneListHtml;
+// }
